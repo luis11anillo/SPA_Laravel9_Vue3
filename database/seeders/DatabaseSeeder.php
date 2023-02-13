95ba91d6d548b33b3513973e39a97548d931c96a
Luis Anillo <luis1104anillo@gmail.com>
2023-02-13 11:18:54 -0500

    Se guardo la configuracion de los archivos al instalar el proyecto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Note;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +18,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+             'email' => 'luis@admin.com',
+             'password' => bcrypt('12345678'),
+         ]);
+
+        Note::factory(30)->create();
     }
 }
