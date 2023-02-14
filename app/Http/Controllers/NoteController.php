04a868e82d71f30d4195f39f140b85d3d81bb00f
Luis Anillo <luis1104anillo@gmail.com>
2023-02-14 17:06:54 -0500

    agg en note.php la configuracion para actualizacion de datos masivos (MODEL), agg en el noteCotroller las configuracion en el update y edit (CONTROLLER)

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -60,7 +60,7 @@ class NoteController extends Controller
      */
     public function edit(Note $note)
     {
-        //
+        return Inertia::render('Notes/Edit', compact('note'));
     }
 
     /**
@@ -72,7 +72,14 @@ class NoteController extends Controller
      */
     public function update(Request $request, Note $note)
     {
-        //
+        $request->validate([
+            'excerpt' => 'required',
+            'content' => 'required',
+        ]);
+
+        $note->update($request->all());
+
+        return redirect()->route('notes.index');
     }
 
     /**
