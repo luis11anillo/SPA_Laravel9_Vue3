71889dfbdddbdd656148ed3e5bffd7c4a685f43c
Luis Anillo <luis1104anillo@gmail.com>
2023-02-16 11:11:49 -0500

    Se ajusto la parte inferior del Edit.vue

--- resources/js/Pages/Notes/Edit.vue
+++ resources/js/Pages/Notes/Edit.vue
@@ -66,8 +66,6 @@ export default {
                                     rows="8">
                                 </textarea>
 
-                                <hr class="my-6">
-
                                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">
                                     Editar
                                 </button>
