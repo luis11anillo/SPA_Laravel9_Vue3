1d9db67b71f5c27770aa870a328ff02947b0df43
Luis Anillo <luis1104anillo@gmail.com>
2023-02-23 15:37:15 -0500

    Agg el mensaje del welcome

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -12,11 +12,11 @@ import AppLayout from '@/Layouts/AppLayout.vue';
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <h1 class="text-8xl font-sans font-bold text-orange-700">WELCOME</h1>
-                <p class="font-sans pt-6 text-white">This is a SPA (Single Page Applications) of notes where you can Create, Edit, Delete and Search notes made by yourself, this project was made with laravel 9 + inertia,
-                    <a href="https://vuejs.org/">vue.js 3</a>  and
-                    <a href="https://tailwindcss.com/">tailwind CSS</a>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 py-20">
+                <h1 class="text-8xl font-sans font-bold text-white">WELCOME</h1>
+                <p class="font-sans pt-6 text-white text-xl">This is a SPA (Single Page Applications) of notes where you can Create, Edit, Delete and Search notes made by yourself in an easy and fast way; this project was made with laravel 9 + inertia,
+                    <a href="https://vuejs.org/" class="hover:text-green-900">vue.js 3</a>  and
+                    <a href="https://tailwindcss.com/" class="hover:text-blue-900">tailwind CSS</a>
                 </p>
             </div>
         </div>
